470c40018fde70be259fd15bbcfaa47da9a45e5a
Francisco Junior <franciscojdsjr@gmail.com>
2024-02-25 08:14:35 -0300

    att

--- app/urls.py
+++ app/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
 
+
 def hello_view(request):
     return JsonResponse({'message':'Hello Word'})
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
